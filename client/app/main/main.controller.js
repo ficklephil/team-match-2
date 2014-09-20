9922000641632032d7f88b1607932cc7e4a18556
Philip <ficklephil@hotmail.com>
2014-09-20 23:55:23 +0100

    Camera and getting colours back is working

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -20,5 +20,67 @@ angular.module('teamMatch2App')
       $http.delete('/api/things/' + thing._id);
     };
 
-    //ok so here let's match the color
-  });
+    $scope.snap = function(){
+
+        var colorThief = new ColorThief();
+
+        context.drawImage(video, 0, 0, 640, 480);
+        var canvasImage = new CanvasImage(canvas);
+
+        console.log(colorThief.getColor(canvasImage));
+        console.log(colorThief.getPalette(canvasImage));
+    };
+
+    //HTML5 VIDEO
+    // Grab elements, create settings, etc.
+    var canvas = document.getElementById("canvas"),
+        context = canvas.getContext("2d"),
+        video = document.getElementById("video"),
+        videoObj = { "video": true },
+        errBack = function(error) {
+            console.log("Video capture error: ", error.code);
+        };
+
+    // Put video listeners into place
+    if(navigator.getUserMedia) { // Standard
+        navigator.getUserMedia(videoObj, function(stream) {
+            video.src = stream;
+            video.play();
+        }, errBack);
+    } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+        navigator.webkitGetUserMedia(videoObj, function(stream){
+            video.src = window.webkitURL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    }
+    else if(navigator.mozGetUserMedia) { // Firefox-prefixed
+        navigator.mozGetUserMedia(videoObj, function(stream){
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        }, errBack);
+    }
+});
+
+
+angular.module('teamMatch2App')
+    .directive('picture', function () {
+
+        console.log('In directive');
+
+        return{
+            restrict: 'A',
+            link: function ($scope, $element) {
+                console.log('In link');
+            },
+            controller: function ($scope, $http, $element) {
+                console.log('In controller');
+
+//                var colorThief = new ColorThief();
+//
+//                var image = new Image();
+//                image.src = $('.fashion-image').attr('src');
+//
+//                colorThief.getColor(image);
+            }
+        };
+    });
